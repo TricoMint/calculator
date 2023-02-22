8b049e291161b15a6a1d5311aeaf5699a256622f
TricoMint <11ldannen@gmail.com>
2023-02-22 02:39:26 -0800

    add basic math functions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function add(num, num2) {
+    let add;
+    add = num + num2;
+    return add
+}
+
+function subtract(num, num2) {
+    let subtract;
+    subtract = num - num2;
+    return subtract
+}
+
+function multiply(num, num2) {
+    let multiply;
+    multiply = num * num2;
+    return multiply;
+}
+
+function divide(num, num2) {
+    let divide;
+    divide = (num / num2);
+    return divide;
+}
